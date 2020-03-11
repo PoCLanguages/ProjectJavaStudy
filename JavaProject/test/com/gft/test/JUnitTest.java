4a1ba4cfced39372e74945940ec7de8bc5fb3cd2
Tan Kin Tat <tankintat@gmail.com>
2020-03-11 16:30:30 +0100

    Actualización.

---
+++ JavaProject/test/com/gft/test/JUnitTest.java
@@ -0,0 +1,13 @@
+package com.gft.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
