aedcea12e3088211cb8c446552d01f894def2a29
Tan Kin Tat <tankintat@gmail.com>
2020-03-12 14:30:59 +0100

    Actualización ficheros.

---
+++ JavaProject/src/com/gft/java/Queues.java
@@ -0,0 +1,63 @@
+package com.gft.java;
+
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Queues {
+
+	public static void main(String[] args) {
+		
+		Queue<Integer> q1 = new ArrayBlockingQueue<>(3);
+		
+		q1.add(10);
+		q1.add(20);
+		q1.add(30);
+		
+		try {
+			q1.add(40);
+		} catch (IllegalStateException e) {
+			System.out.println("Tried to add too many items to the queue");
+		}
+		
+		for (Integer value : q1) {
+			System.out.println("Queue value: " + value);
+		}
+		
+		System.out.println("Remove from queue: " + q1.remove());
+		System.out.println("Remove from queue: " + q1.remove());
+		System.out.println("Remove from queue: " + q1.remove());
+		
+		try {
+			System.out.println("Remove from queue: " + q1.remove());			
+		} catch (NoSuchElementException e) {
+			System.out.println("Tried to remove too many items from queue.");
+		}
+		
+		
+		////////////////////////////////////////////////////////
+		
+		Queue<Integer> q2 = new ArrayBlockingQueue<>(2);
+		
+		System.out.println("Queue 2 peek: " + q2.peek());
+		
+		q2.offer(10);
+		q2.offer(20);
+		
+		System.out.println("Queue 2 peek: " + q2.peek());
+		
+		q2.offer(10);
+		q2.offer(20);
+		if(q2.offer(30) == false)
+			System.out.println("Offer failed to add third item.");
+		
+		for (Integer value : q2) {
+			System.out.println("Queue 2 value: " + value);
+		}
+		
+		System.out.println("Queue 2 poll: " + q2.poll());
+		System.out.println("Queue 2 poll: " + q2.poll());
+		System.out.println("Queue 2 poll: " + q2.poll());
+		
+	}
+}
